017e2c14b93b289d98dc26e2aa32196d80d9a617
xiaoxie <hotcoffie@163.com>
2022-04-24 00:07:18 +0800

    意外把本地调试地址提交，回退

--- web_src/config/index.js
+++ web_src/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/debug': {
-        target: 'http://172.18.0.61:18088/',
+        target: 'http://localhost:18088/',
         changeOrigin: true,
         pathRewrite: {
           '^/debug': '/'
