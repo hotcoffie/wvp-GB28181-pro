f8d60aa93012b31048c0258493f5a6b1d7055461
xiaoxie <hotcoffie@163.com>
2022-05-01 21:20:40 +0800

    增加打包脚本

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 call git checkout smart-box
 call:build
 COPY D:\workspace\smart-box-vedio\target\wvp-pro.jar wvp-pro.jar
-echo 完成wvp-pro打包
+echo '完成wvp-pro打包'
 
 call git checkout wvp-28181-2.0
 call:build
 COPY D:\workspace\smart-box-vedio\target\wvp-*.jar wvp.jar
-echo 完成wvp打包
+echo '完成wvp打包'
 goto:end
 
 :build
